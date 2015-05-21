3a98f8732b4fa7263a86ab52bfc71a49cf271d7a
rua8007 <ranaumair8007@gmal.com>
2015-05-21 16:57:23 +0500

    Finish Layouts and Routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,10 @@
 module ApplicationHelper
-	def full_tiltle(base_title)
-		base_title="RoR Sample App"
-		if page_title.is.empty?
-			base_title
-		else
-			"#{base_title}"|"#{page_title}"
-
-		end
-	end	
+	def full_title(page_title)
+	base_title = "RoR Sample App"
+	if page_title.empty?
+	base_title
+	else
+	"#{base_title} | #{page_title}"
+	end
+	end
 end
